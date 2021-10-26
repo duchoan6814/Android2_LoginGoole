2667fb9a72eac8758a9106f72f6a2a8e6434a6d0
Hoan Truong <hoantruong6814@gmail.com>
2021-10-26 14:37:01 +0700

    done feat login with google

---
+++ app/src/main/java/com/example/projectlogingoogle/MainActivity.java
@@ -0,0 +1,99 @@
+package com.example.projectlogingoogle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GoogleSignInClient mGoogleSignInClient;
+    private SignInButton signInButton;
+    private TextView txtEmail;
+    private Button btnLogout;
+    private int RC_SIGN_IN = 1;
+    private String TAG = "Main screen";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        txtEmail = findViewById(R.id.txtEmail);
+        btnLogout = findViewById(R.id.btnLogout);
+
+
+        signInButton = findViewById(R.id.btnSignIn);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+
+
+        signInButton.setOnClickListener(view -> {
+            signIn();
+        });
+
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> task) {
+        try {
+            GoogleSignInAccount account = task.getResult(ApiException.class);
+            updateUI(account);
+        }catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            updateUI(null);
+        }
+    }
+
+    private void signIn() {
+        Intent intent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(intent, RC_SIGN_IN);
+    }
+
+
+    private void updateUI(GoogleSignInAccount account) {
+        if (account == null) {
+            signInButton.setEnabled(true);
+            btnLogout.setEnabled(false);
+        }
+
+        System.out.println("account " + account.getEmail());
+        txtEmail.setText(account.getEmail().toLowerCase());
+
+
+
+    }
+}
